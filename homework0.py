201a6501e086014cd7d48cdb8667e6fe788adec9
melnikvladim1r <162798416+melnikvladim1r@users.noreply.github.com>
2024-03-12 19:08:37 +0300

    Add files via upload

---
+++ homework0.py
@@ -0,0 +1,10 @@
+name = "Vladimir" # я так понял можно любое слово использовать
+print(name)
+print(name [0])
+print(name[-1])
+print(name[2:5])
+print(name[::-1])
+print(len(name))
+str = ' это новая строка'
+print(str)
+print(name + str)
